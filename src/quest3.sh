f7b523753f70ed82da3e6a47e1a596961ac1cbf9
Tamela Bears <tamelabe@ev-e6.kzn.21-school.ru>
2022-05-30 15:08:44 +0300

    Init commit

---
+++ src/quest3.sh
@@ -0,0 +1,10 @@
+chmod u+x ai_door_managment_module.sh
+mv door_managment_fi door_managment_files
+cd door_managment_files
+mkdir door_configuration
+mkdir door_logs
+mkdir door_map
+mv *.log door_logs
+mv *.conf door_configuration
+mv door_map_1.1 door_map
+
---
+++ src/quest6.sh
@@ -0,0 +1,10 @@
+cd ai_help
+ls
+chmod u+x keygen.sh
+./keygen.sh
+cd key
+ls
+rm file*
+cd ..
+chmod u+x unifier.sh
+./unifier.sh
